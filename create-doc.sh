ad570852432ab88fb6c04ff8858948e729118faa
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-05 23:25:09 +0200

    Clean Stats/Bootstrap/Error

---
+++ create-doc.sh
@@ -0,0 +1,1 @@
+~/web/vendor/bin/phpdoc.php -d ~/web/ -t ~/doc/ -i /var/www/clients/client12/web70/web/vendor/
--- template/footer.php
+++ template/footer.php
@@ -1,8 +1,18 @@
 <div id="footerWrapper">
-	<div id="footerDeko"></div>
-    
+	<div id="footerKontakt"></div>
+    <div id="footerUp"></div>  
+    <div id="footerImpressum"></div>  
     <div id="footerContent">
-    	footer
+    	<div id="footerLinks">
+        	<a href="../../index.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/indoor.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/outdoor.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/anmeldung.php">Anmeldung und Registrierung</a><br />
+          	<a href="../../template/profil.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/warenkorb.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/kontakt.php">Anmeldung und Registrierung</a><br />
+            <a href="../../template/impressum.php">Anmeldung und Registrierung</a><br />
+        </div>
     </div>
 
 </div>
