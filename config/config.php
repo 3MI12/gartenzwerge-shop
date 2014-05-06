d0e94ff7678d27f465e268a89e5b51323c5085eb
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-06 16:16:44 +0200

    Config

--- config/config.php
+++ config/config.php
@@ -3,3 +3,4 @@
 define('THEME_PATH', 'themes/gartenzwerge/');
 define('TEMPLATE_PATH', THEME_PATH.'templates/');
 define('FE_THEME_PATH', '/themes/gartenzwerge/');
+define('SALT', '$6$rounds=10000$dsaDSDNjsnksdnSND823NDudjsdjIWEDHSdhksdsskadiusyriweadnsjADSSAKD');
