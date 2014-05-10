e378e0cc89d77f59aa4bbf30cb66f6c2f5649e41
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-10 18:55:25 +0200

    Checkbox

--- themes/gartenzwerge/js/script.js
+++ themes/gartenzwerge/js/script.js
@@ -18,4 +18,32 @@ $("#footerDown").click(function(){
 $(".btn-articleText").click(function(){
 	$(this).parent("form").children(".articleText").toggle(1000);
 });
+
+$("#checkboxStatus").click(function(){
+	if( $(this).prop("checked") == true ){
+		$(this).prop("checked" , false);
+		$("#checkboxStatus label").animate( { left: 7, backgroundColor: "red" }, "slow");
+		}
+		else {
+			$(this).prop("checked" , true);
+			$("#checkboxStatus label").animate( { left: 40, backgroundColor: "white" }, "slow");
+			}
+	})
+
+$("#checkboxAdmin").click(function(){
+	if( $(this).prop("checked") == true ){
+		$(this).prop("checked" , false);
+		$("#checkboxAdmin label").animate( { left: 7, backgroundColor: "red" }, "slow");
+		}
+		else {
+			$(this).prop("checked" , true);
+			$("#checkboxAdmin label").animate( { left: 40, backgroundColor: "white" }, "slow");
+			}
+	
+
+	
+/*	var c = this.checked ? '7' : '40';
+    $("#checkboxStatus label").animate( { left: c }, "slow");*/
+	})
+
 });
