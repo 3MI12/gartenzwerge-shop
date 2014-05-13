bd8bfcff3eee1511cfa642f00eb2b6f7bacbb256
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-13 19:42:36 +0200

    mediaManager

--- config/config.php
+++ config/config.php
@@ -4,6 +4,8 @@ define('THEME_PATH', 'themes/gartenzwerge/');
 define('TEMPLATE_PATH', THEME_PATH.'templates/');
 define('FE_THEME_PATH', '/themes/gartenzwerge/');
 define('SALT', '$6$rounds=10000$dsaDSDNjsnksdnSND823NDudjsdjIWEDHSdhksdsskadiusyriweadnsjADSSAKD');
-
+define('IMAGE_SMALL', '120');
+define('IMAGE_MEDIUM', '260');
+define('IMAGE_BIG', '800');
 define('SHIPPING_FEE', 4);
 define('SHIPPING_FREE_FROM', 30);
