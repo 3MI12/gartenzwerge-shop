39529a652b25b7bcc266f6bd50fc82a10eef4dbb
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-19 11:44:02 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,3 +11,6 @@ define('IMAGE_BIG', '800');
 define('SHIPPING_FEE', 4);
 define('SHIPPING_FREE_FROM', 30);
 define('DATE_FORMAT', 'Y-m-d H:i:s');
+define('SENDER_MAIL', 'FROM: Gartenzwerge24.eu <info@gartenzwerge24.eu>');
+define('SUBJECT_NEWUSER', 'Neues Nutzerkonto auf Gartenzwerge24.eu!');
+define('MESSAGE_NEWUSER', ', ihr Nutzerkonto wurde erfolgreich auf Gartenzwerge24.eu angelegt!');
