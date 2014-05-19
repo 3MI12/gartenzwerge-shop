e75a5d3ee061eac5a426c16459bd1b3d1e9cedbe
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-19 11:06:28 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@ require_once("vendor/autoload.php");
 require_once("config/dbParms.php");
 require_once 'config/config.php';
 require_once 'shop/shopHelper.php';
-//require_once 'shop/mediaManager.php';
+require_once 'shop/mediaManager.php';
 require_once 'shop/entity/Article.php';
 require_once 'shop/entity/OrderArticle.php';
 require_once 'shop/entity/User.php';
