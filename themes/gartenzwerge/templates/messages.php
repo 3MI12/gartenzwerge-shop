da1cdb2b87eecfe7f61008f337a8b67331d75e75
Benjamin Brandt <benjamin.brandt@verbunden.net>
2014-05-22 08:30:59 +0200

    Update messages.php

--- themes/gartenzwerge/templates/messages.php
+++ themes/gartenzwerge/templates/messages.php
@@ -1,13 +1,10 @@
 <?php if(!empty($_SESSION['messages'])): ?>
 <div id="errorBox">
+<div><a>X</a></div>
 	<ul>
-
 	<?php foreach($_SESSION['messages'] as $message): ?>
-
 		<li><?php echo htmlspecialchars($message); ?></li>
-
 	<?php endforeach; ?>
-
 	</ul>
 </div>
 <?php $_SESSION['messages'] = null; endif; ?>
