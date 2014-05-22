d7b4ebef9bebb823042dac62b60a12841b251187
n0fr3akz <xaver.schreiber@googlemail.com>
2014-05-22 06:44:46 +0200

    Update impressum.php
    Übersichtlichkeit AGB

--- themes/gartenzwerge/templates/impressum.php
+++ themes/gartenzwerge/templates/impressum.php
@@ -20,20 +20,23 @@ eMail: admin@gartenzwerge24.eu<br />
     </div>
     <div class="titleHr"></div>
 <p style="clear:both;">
-1. Zustandekommen des Vertrags Mit Ihrer Bestellung geben Sie ein verbindliches Angebot an uns ab, einen Vertrag mit Ihnen zu schließen. Mit der Zusendung einer Auftragsbestätigung per E-Mail an Sie oder der Lieferung der bestellten Ware können wir dieses Angebot annehmen. Zunächst erhalten Sie eine Bestätigung des Eingangs Ihrer Bestellung per E-Mail an die von Ihnen angegebene E-Mail-Adresse (Bestellbestätigung). Ein Kaufvertrag kommt jedoch erst mit dem Versand unserer Auftragsbestätigung per E-Mail an Sie oder mit der Lieferung der bestellten Ware zustande.
-Bei Eingang einer Bestellung in unserem Internetshop gelten folgende Regelungen: Der Verbraucher gibt ein bindendes Vertragsangebot ab, indem er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich durchläuft. Die Bestellung erfolgt in folgenden Schritten:
-1) Auswahl der gewünschten Ware 2) Bestätigen durch Anklicken der Buttons „Bestellen“ 3) Prüfung der Angaben im Warenkorb 4) Anmeldung im Internetshop nach Registrierung und Eingabe der Anmelderangaben (E-Mail-Adresse und Passwort). 5) Verbindliche Absendung der Bestellung.<br />
+1. Zustandekommen des Vertrags <br />
+Mit Ihrer Bestellung geben Sie ein verbindliches Angebot an uns ab, einen Vertrag mit Ihnen zu schließen. Mit der Zusendung einer Auftragsbestätigung per E-Mail an Sie oder der Lieferung der bestellten Ware können wir dieses Angebot annehmen. Zunächst erhalten Sie eine Bestätigung des Eingangs Ihrer Bestellung per E-Mail an die von Ihnen angegebene E-Mail-Adresse (Bestellbestätigung). Ein Kaufvertrag kommt jedoch erst mit dem Versand unserer Auftragsbestätigung per E-Mail an Sie oder mit der Lieferung der bestellten Ware zustande.
+Bei Eingang einer Bestellung in unserem Internetshop gelten folgende Regelungen: Der Verbraucher gibt ein bindendes Vertragsangebot ab, indem er die in unserem Internetshop vorgesehene Bestellprozedur erfolgreich durchläuft. Die Bestellung erfolgt in folgenden Schritten:<br />
+1) Auswahl der gewünschten Ware<br /> 2) Prüfung der Angaben im Warenkorb <br />3) Anmeldung im Internetshop nach Registrierung und Eingabe der Anmelderangaben (E-Mail-Adresse und Passwort). <br />4) Verbindliche Absendung der Bestellung.<br />
 <br />
 
-2. Speicherung des Vertragstextes Den Vertragstext Ihrer Bestellung speichern wir. Ihre vergangenen Bestellungen können Sie in unserem Kunden-Bereich unter Mein Profil --> Meine Bestellungen einsehen. Wir senden Ihnen außerdem eine Bestellbestätigung sowie eine Auftragsbestätigung mit allen Bestelldaten und unseren Allgemeinen Geschäftsbedingungen an die von Ihnen angegebene E-Mail-Adresse.<br />
+2. Speicherung des Vertragstextes <br />
+Den Vertragstext Ihrer Bestellung speichern wir. Ihre vergangenen Bestellungen können Sie in unserem Kunden-Bereich unter Mein Profil --> Meine Bestellungen einsehen. Wir senden Ihnen außerdem eine Bestellbestätigung sowie eine Auftragsbestätigung mit allen Bestelldaten und unseren Allgemeinen Geschäftsbedingungen an die von Ihnen angegebene E-Mail-Adresse.<br />
 <br />
 
-3. Eigentumsvorbehalt Die gelieferte Ware bleibt bis zur vollständigen Bezahlung aller Forderungen unser Eigentum.<br />
+3. Eigentumsvorbehalt <br />
+Die gelieferte Ware bleibt bis zur vollständigen Bezahlung aller Forderungen unser Eigentum.<br />
 <br />
 
-4. Preise, Versandkosten, Rücksendekosten bei Widerruf
+4. Preise, Versandkosten, Rücksendekosten bei Widerruf<br />
 Die angegebenen Preise enthalten die gesetzliche Umsatzsteuer und sonstige Preisbestandteile.
-Hinzu kommen etwaige Versandkosten.
+Hinzu kommen etwaige Versandkosten.<br />
 Soweit Sie von einem bestehenden Widerrufsrecht Gebrauch machen, haben Sie die regelmäßigen
 Kosten der Rücksendung zu tragen, wenn die gelieferte Ware der bestellten entspricht und wenn der
 Preis der zurückzusendenden Sache einen Betrag von 40 Euro nicht übersteigt oder wenn Sie bei
@@ -42,46 +45,46 @@ vertraglich vereinbarte Teilzahlung erbracht haben. Anderenfalls ist die Rückse
 kostenfrei.<br />
 <br />
 
-5. Lieferbedingungen
+5. Lieferbedingungen<br />
 Sofern nicht beim Angebot anders angegeben, bringen wir die Ware innerhalb von 2 Werktagen nach
 Zahlungseingang in den Versand.<br />
 <br />
 
-6. Zahlungsbedingungen
+6. Zahlungsbedingungen<br />
 Der Verbraucher hat die Möglichkeit der Zahlung per Bankeinzug.<br />
 <br />
 
-7. Gewährleistung
+7. Gewährleistung<br />
 Die Gewährleistung richtet sich nach den gesetzlichen Bestimmungen. Soweit gebrauchte Waren
 Gegenstand des Kaufvertrags sind und der Käufer nicht Verbraucher ist, wird die Gewährleistung
 ausgeschlossen. Ist der Kunde Verbraucher, beträgt die Gewährleistungsfrist beim Kauf gebrauchter
 Sachen ein Jahr.<br />
 <br />
 
-8. Datenschutz
+8. Datenschutz<br />
 Bei Anbahnung, Abschluss, Abwicklung und Rückabwicklung eines Kaufvertrages werden von uns
 Daten im Rahmen der gesetzlichen Bestimmungen erhoben, gespeichert und verarbeitet.
 Beim Besuch unseres Internetangebots werden die aktuell von Ihrem PC verwendete IP-Adresse,
 Datum und Uhrzeit, der Browsertyp und das Betriebssystem Ihres PC sowie die von Ihnen
 betrachteten Seiten protokolliert. Rückschlüsse auf personenbezogene Daten sind uns damit jedoch
-nicht möglich und auch nicht beabsichtigt.
+nicht möglich und auch nicht beabsichtigt.<br />
 Die personenbezogenen Daten, die Sie uns z. B. bei einer Bestellung oder per E-Mail mitteilen (z. B.
 Ihr Name und Ihre Kontaktdaten), werden nur zur Korrespondenz mit Ihnen und nur für den Zweck
 verarbeitet, zu dem Sie uns die Daten zur Verfügung gestellt haben. Wir geben Ihre Daten nur an das
 mit der Lieferung beauftragte Versandunternehmen weiter, soweit dies zur Lieferung der Waren
 notwendig ist. Zur Abwicklung von Zahlungen geben wir Ihre Zahlungsdaten an das mit der Zahlung
-beauftragte Kreditinstitut weiter.
+beauftragte Kreditinstitut weiter.<br /><br />
 Wir versichern, dass wir Ihre personenbezogenen Daten im Übrigen nicht an Dritte weitergeben, es
 sei denn, dass wir dazu gesetzlich verpflichtet wären oder Sie vorher ausdrücklich eingewilligt haben.
 Soweit wir zur Durchführung und Abwicklung von Verarbeitungsprozessen Dienstleistungen Dritter in
 Anspruch nehmen, werden die Bestimmungen des Bundesdatenschutzgesetzes eingehalten.
-Dauer der Speicherung
+Dauer der Speicherung<br />
 Personenbezogene Daten, die uns über unsere Website mitgeteilt worden sind, werden nur so lange
 gespeichert, bis der Zweck erfüllt ist, zu dem sie uns anvertraut wurden. Soweit handels- und
 steuerrechtliche Aufbewahrungsfristen zu beachten sind, kann die Dauer der Speicherung
-bestimmter Daten bis zu 10 Jahre betragen.
-Ihre Rechte Sollten Sie mit der Speicherung Ihrer personenbezogenen Daten nicht mehr einverstanden oder diese unrichtig geworden sein, werden wir auf eine entsprechende Weisung hin im Rahmen der gesetzlichen Bestimmungen die Löschung, Korrektur oder Sperrung Ihrer Daten veranlassen. Auf Wunsch erhalten Sie unentgeltlich Auskunft über alle personenbezogenen Daten, die wir über Sie gespeichert haben. Bei Fragen zur Erhebung, Verarbeitung oder Nutzung Ihrer personenbezogenen Daten, für Auskünfte, Berichtigung, Sperrung oder Löschung von Daten wenden Sie sich bitte an:
-Gartenzwerge24 Steve Kuntze Hechtstraße 40 D-01097 Dresden E-Mail admin@gartenzwerge24.eu
+bestimmter Daten bis zu 10 Jahre betragen.<br />
+Ihre Rechte Sollten Sie mit der Speicherung Ihrer personenbezogenen Daten nicht mehr einverstanden oder diese unrichtig geworden sein, werden wir auf eine entsprechende Weisung hin im Rahmen der gesetzlichen Bestimmungen die Löschung, Korrektur oder Sperrung Ihrer Daten veranlassen. Auf Wunsch erhalten Sie unentgeltlich Auskunft über alle personenbezogenen Daten, die wir über Sie gespeichert haben.<br /> Bei Fragen zur Erhebung, Verarbeitung oder Nutzung Ihrer personenbezogenen Daten, für Auskünfte, Berichtigung, Sperrung oder Löschung von Daten wenden Sie sich bitte an:
+Gartenzwerge24<br /> Steve Kuntze<br /> Hechtstraße 40<br /><br /> D-01097 Dresden<br /> E-Mail admin@gartenzwerge24.eu<br />
 Links auf andere Internetseiten Soweit wir von unserem Internetangebot auf die Webseiten Dritter verweisen oder verlinken, können wir keine Gewähr und Haftung für die Richtigkeit bzw. Vollständigkeit der Inhalte und die Datensicherheit dieser Websites übernehmen. Da wir keinen Einfluss auf die Einhaltung datenschutzrechtlicher Bestimmungen durch Dritte haben, sollten Sie die jeweils angebotenen Datenschutzerklärungen gesondert prüfen.<br />
 <br />
 
